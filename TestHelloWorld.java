e30f7227c4a813672f5ec10e41337d53066568d2
Rahul Tarsolia <Rahul.Tarsolia@synechron.com>
2019-05-07 11:44:04 -0400

    adding java helloworld

---
+++ TestHelloWorld.java
@@ -0,0 +1,11 @@
+public class TestHelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(" This is Hello World Java program for Jenkin Integration.");
+
+	}
+
+}
